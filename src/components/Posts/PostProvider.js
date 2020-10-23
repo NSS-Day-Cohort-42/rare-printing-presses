0bbea72da3922fce4aa8000bb830cc122b0cfb3d
LukeZvada <zvadamusic@gmail.com>
2020-10-23 08:26:15 -0700

    added post provider

---
+++ src/components/Posts/PostProvider.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+export const PostContext = React.createContext()
+
+export const PostsProvider = (props) => {
+    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState([])
+
+    const getAllPosts = () => {
+        return fetch(`http://localhost:8088/posts`)
+            .then(res => res.json())
+            .then(setPosts)
+    }
+
+    const getSinglePost = (post) => {
+        return fetch(`http://localhost:8088/posts/${post}`)
+            .then(res => res.json())
+            .then(setPost)
+    }
+
+    return (
+        <PostContext.Provider value={{
+            posts, getAllPosts, post, getSinglePost
+        }}>
+            {props.children}
+        </PostContext.Provider>
+    )
+}
