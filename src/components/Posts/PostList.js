16e43b1d5021f4d1963588999318dfeef7162234
LukeZvada <zvadamusic@gmail.com>
2020-10-26 13:26:31 -0700

    adding boiler logic for changing user id to username

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { PostContext } from "./PostProvider" 
 import {CategoryContext} from "../categories/CategoriesProvider"
+import {users} from "../auth/AuthProvider"
 import "./Post.css"
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -50,6 +51,7 @@ export const PostList = (props) => {
                 {
                     posts.map(post => {
                         const category = categories.find(c => c.id === post.category_id) || {}
+                        // const userName = users.find(c => c.id === post.category_id) || {}
                         return <section key={post.id} className="posts">
                                         <div className="PostAuthor">Author: {post.user_id} </div>
                                         <div className="PostTitle">Title: {post.title} </div>
