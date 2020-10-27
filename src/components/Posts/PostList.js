15fb136b1738d2b6fe9c4ac8bb43f6b7546b941d
mswinkels09 <mswinkels09@gmail.com>
2020-10-27 09:07:30 -0500

    merge

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -12,6 +12,9 @@ export const PostList = (props) => {
     const { posts, getAllPosts } = useContext(PostContext)
     const { categories, getAllCategories} = useContext(CategoryContext)
 
+    const [filteredPosts, setFiltered] = useState([])
+    const [categorySelected, setCategorySelected] = useState([])
+
     useEffect(() => {
         getAllPosts()
             .then(getAllCategories)
@@ -38,6 +41,13 @@ export const PostList = (props) => {
 
     const classes = useStyles()
 
+    const filterPosts = (event) => {
+        const filteredByCategories = posts.filter(p => p.category_id === event.target.value)
+        setFiltered(filteredByCategories)
+        setCategorySelected(parseInt(event.target.value))
+
+    }
+
     return (
         <>
             {/* <article className="welcomeMessage">
@@ -45,11 +55,23 @@ export const PostList = (props) => {
                     <div><h1 className="welcomeTitle">Hey, {currentUser.firstName}</h1></div>
                 </section>
             </article> */}
-            
+            <h1>Posts</h1>
+            <section className="filteredPosts">
+                <label htmlFor="category_id">Filter By Category</label>
+                    <select name="category_id" className="form-control" 
+                    value={categorySelected.id} onChange={filterPosts}>
+                        <option value="0">Select a Category</option>
+                        {categories.map(c => (
+                            <option key={c.id} value={c.id}>
+                                {c.label}
+                            </option>
+                        ))}
+                    </select>
+            </section>
 
             <article className="postsContainer">
                 {
-                    posts.map(post => {
+                    filteredPosts.map(post => {
                         const category = categories.find(c => c.id === post.category_id) || {}
                         // const userName = users.find(c => c.id === post.category_id) || {}
                         return <section key={post.id} className="posts">
