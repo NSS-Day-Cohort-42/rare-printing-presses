6c3d41e469d98d6e41a7dc67a505313426f19cf3
Johnson4996 <Johnson.4996@gmail.com>
2020-11-12 14:08:56 -0600

    minor change

--- src/components/categories/CategoryEdit.js
+++ src/components/categories/CategoryEdit.js
@@ -6,6 +6,7 @@ export const CategoryEdit = (props)=>{
     const [category, setCategory] = useState({})
 
     useEffect(() => {
+        getAllCategories()
         getCategoryInEditMode()
     }, [])
 
