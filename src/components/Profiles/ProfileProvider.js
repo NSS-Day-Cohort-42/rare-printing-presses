e855992f7d0d0a6ae167b4457e70bb88f0bf0eb8
LukeZvada <zvadamusic@gmail.com>
2020-11-16 09:51:08 -0600

    added getAllProfiles and getSingleProfile methods in ProfileProvider

--- src/components/Profiles/ProfileProvider.js
+++ src/components/Profiles/ProfileProvider.js
@@ -5,4 +5,29 @@ export const ProfileContext = React.createContext()
 export const ProfileProvider = (props) => {
     const [profile, setProfile] = useState([])
 
+    const getAllProfiles = () => {
+        return fetch(`http://localhost:8000/profile`, {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rareUser_id")}`
+            }})
+            .then(res => res.json())
+            .then(setProfile)
+    }
+
+    const getSingleProfile = (profile) => {
+        return fetch(`http://localhost:8000/profile/${profile}`, {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rareUser_id")}`
+            }})
+            .then(res => res.json())
+            .then(setProfile)
+    }
+
+    return (
+        <ProfileContext.Provider value={{
+            getAllProfiles, profile, getSingleProfile
+        }}>
+            {props.children}
+        </ProfileContext.Provider>
+    )
 }
