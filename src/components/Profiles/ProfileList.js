218ac1d390bee9e8eb8e68ddd8627534a7578593
LukeZvada <zvadamusic@gmail.com>
2020-11-16 11:13:41 -0600

    added if/else to handle create/deactivate admins

--- src/components/Profiles/ProfileList.js
+++ src/components/Profiles/ProfileList.js
@@ -11,18 +11,29 @@ export const ProfileList = (props) => {
         getAllProfiles()
     }, [])
 
+
     return (
         <article className="profileContainer">
         {
             profile.map(p => {
-                
-                return <section key={p.id} className="profiles">
+                if(p.user.is_staff){
+
+                    return <section key={p.id} className="profiles">
                             <div className="profile-info">
                                 <div className="profileUsername">{p.user.username}</div>
                                 <div className="profileFullName">{p.user.first_name} {p.user.last_name}</div>
-                                <div className="profileUsertype">{p.usertype}</div>
+                                <div className="profile_Is_Staff">{p.user.is_staff}<button>Make Admin</button></div>
+                                <div className="profile_Is_Staff">{p.user.is_staff}<button>Deactivate Account</button></div>
                             </div>
                         </section>
+                } else {
+                    return <section key={p.id} className="profiles">
+                                <div className="profile-info">
+                                    <div className="profileUsername">{p.user.username}</div>
+                                    <div className="profileFullName">{p.user.first_name} {p.user.last_name}</div>
+                                </div>
+                            </section>
+                }
             })
         }
     </article>
