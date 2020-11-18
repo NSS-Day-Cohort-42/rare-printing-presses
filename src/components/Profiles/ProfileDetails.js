bc3942ea255d6013c9d7a000401f969a013892db
LukeZvada <zvadamusic@gmail.com>
2020-11-18 15:19:14 -0600

    upload image buttons only appear on user owner profile

--- src/components/Profiles/ProfileDetails.js
+++ src/components/Profiles/ProfileDetails.js
@@ -11,6 +11,7 @@ export const ProfileDetails = (props) => {
     var pathArray = window.location.pathname.split('/')
     let profileNumber = parseInt(pathArray[2])
 
+
     useEffect(() => {
         getSingleRareProfile(profileNumber)
     }, [])
@@ -22,46 +23,53 @@ export const ProfileDetails = (props) => {
     }
     
     const createProfileImageJSON = (event) => {
-        debugger
+        
         getBase64(event.target.files[0], (base64ImageString) => {
-            // console.log("Base64 of file is", base64ImageString);
+            // console.log("Base64  of file is", base64ImageString);
             setImageUrl(base64ImageString)
         });
     }
 
-    const currentUser = localStorage.getItem("rareUser_number")
+    const currentUser = parseInt(localStorage.getItem("rareUser_number"))
 
-    const postImage = () => { 
+    const postImage = (imgstring) => { 
             return fetch(`http://localhost:8000/rareprofile/${currentUser}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": `Token ${localStorage.getItem("rareUser_id")}`
                 },
+                body: JSON.stringify(imgstring)
     })
 }
-
     return (
         <>
         <article className="profileContainer">
             <section key={rareSingleProfile.id} className="profiles">
                 <div className="profile-info">
                     <div className="profileFullName">{rareSingleProfile.user.first_name} {rareSingleProfile.user.last_name}</div>
-                    <div className="profileImg">{rareSingleProfile.profile_image_url}Image</div>
+                    <img src={rareSingleProfile.profile_image_url}></img>
                     <div className="profileUsername">{rareSingleProfile.user.username}</div>
                     <div className="profile_Is_Staff">{rareSingleProfile.user.is_staff}</div>
                     <div className="profile_Is_Staff">{rareSingleProfile.created_on}</div>
                     {
                         (rareSingleProfile.user.is_staff === true) ? <div className="profile_Is_Staff">Admin</div> 
                         : <div className="profile_Is_Staff">Author</div>
+                    
+                    
+
                     }
-                    <input type="file" id="profile_image" onChange={(evt) => {
-                            createProfileImageJSON(evt)
-                        }} />
-                    <input type="hidden" name="profile_image" value={rareSingleProfile.profile_image_url} />
-                        <button onClick={(evt) => {
-                            postImage()
-                        }}>Upload</button>
+                    {
+                        
+                        (rareSingleProfile.id === currentUser) 
+                        ? 
+                        <div className="uploadButtons">
+                        <input type="file" id="profile_image_url" onChange={(evt) => {createProfileImageJSON(evt)}} />
+                        <input type="hidden" name="profile_image_url" value={rareSingleProfile.id} />
+                        <button onClick={(evt) => {postImage({'profile_image_url':imageUrl, "profile_id": rareSingleProfile.id})}}>Upload</button>
+                        </div>
+                        : null
+                    }
                 </div>
             </section>
 
